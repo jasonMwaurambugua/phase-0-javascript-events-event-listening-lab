0f50b426e07b9e86762f8e9a475f480362e4e03b
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-26 17:21:21 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+// Event listening lab
+// Jason
+
+
+
 function addingEventListener() {
 }
+
+
+return "clicked";
+
+function preventingDefault() {
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented')
+    return e.preventDefault()
+  } else {
+    console.log('Sinon')
+  }
+})
+}
+
+function stoppingPropagation(){
+const divs = document.querySelectorAll('div');
+function bubble(e) {
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+}
+
+
